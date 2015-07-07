1b4f45c3f1ebce06278f191e6a8877352e7c55ce
henjue <henjue@ketie.me>
2015-07-07 18:58:59 +0800

    update

---
+++ app/src/main/java/maimeng/yodian/app/client/android/WebLauncher.java
@@ -0,0 +1,63 @@
+package maimeng.yodian.app.client.android;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Set;
+
+
+public class WebLauncher extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_launcher);
+        onNewIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        String scheme = intent.getScheme();
+        Uri uri = intent.getData();
+        StringBuffer sb=new StringBuffer("scheme:" + scheme).append("\n");
+        if (uri != null) {
+            String host = uri.getHost();
+            String path = uri.getPath();
+            sb.append("host"+host).append("\n");
+            sb.append("path"+path).append("\n");
+            Set<String> params = uri.getQueryParameterNames();
+            for(String key:params){
+                sb.append(key+":"+uri.getQueryParameter(key)).append("\n");
+            }
+            ((TextView)findViewById(R.id.text)).setText(sb.toString());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_web_launcher, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
