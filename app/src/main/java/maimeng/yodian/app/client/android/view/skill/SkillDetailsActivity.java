210a7ce530e6f24c6bf3f48963a6c30287702b48
henjue <henjue@gmail.com>
2015-07-27 19:26:29 +0800

    update

--- app/src/main/java/maimeng/yodian/app/client/android/view/skill/SkillDetailsActivity.java
+++ app/src/main/java/maimeng/yodian/app/client/android/view/skill/SkillDetailsActivity.java
@@ -23,21 +23,9 @@ public class SkillDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skill_details);
-        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
+
         //使用CollapsingToolbarLayout必须把title设置到CollapsingToolbarLayout上，设置到Toolbar上则不会显示
-        CollapsingToolbarLayout mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout);
-        mCollapsingToolbarLayout.setTitle("CollapsingToolbarLayout");
         //通过CollapsingToolbarLayout修改字体颜色
-        mCollapsingToolbarLayout.setExpandedTitleColor(Color.WHITE);//设置还没收缩时状态下字体颜色
-        mCollapsingToolbarLayout.setCollapsedTitleTextColor(Color.GREEN);//设置收缩后Toolbar上字体的颜色
         mList=(RecyclerView)findViewById(R.id.recyclerView);
         mList.setLayoutManager(new LinearLayoutManager(this));
         mList.setAdapter(new RecyclerView.Adapter() {
