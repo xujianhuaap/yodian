2a77413ccb64a78b60e512e29506ca60191d850b
henjue <henjue@gmail.com>
2015-07-28 16:26:06 +0800

    update

--- app/src/main/java/maimeng/yodian/app/client/android/view/skill/SkillDetailsActivity.java
+++ app/src/main/java/maimeng/yodian/app/client/android/view/skill/SkillDetailsActivity.java
@@ -1,32 +1,57 @@
 package maimeng.yodian.app.client.android.view.skill;
 
-import android.graphics.Color;
+import android.graphics.RectF;
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import maimeng.yodian.app.client.android.R;
 
 /**
  * Created by android on 2015/7/22.
  */
-public class SkillDetailsActivity extends AppCompatActivity {
+public class SkillDetailsActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener {
+    private static final String LOG_TAG = SkillDetailsActivity.class.getName();
+    @Bind(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+    @Bind(R.id.scrollView)
+    CoordinatorLayout mScrollView;
+    @Bind(R.id.appBarLayout)
+    AppBarLayout mAppBarLayout;
+    @Bind(R.id.title_containar)
+    FrameLayout mTitleContainar;
+    @Bind(R.id.content_containar)
+    FrameLayout mContentContainar;//装着顶部详情的容器
+    @Bind(R.id.wechat_icon)
+    ImageView mWechatIcon;
+    @Bind(R.id.title_wechat_icon)
+    ImageView mTitleWechatIcon;
     private RecyclerView mList;
+    private RectF mRect1 = new RectF();
+    private RectF mRect2 = new RectF();
+    private AccelerateDecelerateInterpolator mSmoothInterpolator;
+    private float mMinHeaderTranslation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skill_details);
-
-        //使用CollapsingToolbarLayout必须把title设置到CollapsingToolbarLayout上，设置到Toolbar上则不会显示
-        //通过CollapsingToolbarLayout修改字体颜色
-        mList=(RecyclerView)findViewById(R.id.recyclerView);
+        mSmoothInterpolator = new AccelerateDecelerateInterpolator();
+        ButterKnife.bind(this);
+        mMinHeaderTranslation = -TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,180,getResources().getDisplayMetrics()) + getResources().getDimensionPixelSize(R.dimen.actionBarSize);
+        mList = (RecyclerView) findViewById(R.id.recyclerView);
         mList.setLayoutManager(new LinearLayoutManager(this));
         mList.setAdapter(new RecyclerView.Adapter() {
             @Override
@@ -36,7 +61,7 @@ public class SkillDetailsActivity extends AppCompatActivity {
 
             @Override
             public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-                ((TextView)holder.itemView).setText("test"+position);
+                ((TextView) holder.itemView).setText("test" + position);
             }
 
             @Override
@@ -44,8 +69,16 @@ public class SkillDetailsActivity extends AppCompatActivity {
                 return 100;
             }
         });
+        mAppBarLayout.addOnOffsetChangedListener(this);
     }
-    class ViewHolder extends RecyclerView.ViewHolder{
+
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int offset) {
+
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
 
         public ViewHolder(View itemView) {
             super(itemView);
