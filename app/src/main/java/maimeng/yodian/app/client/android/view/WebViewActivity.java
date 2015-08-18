db8aacd95d181f2e336b1b1203d47322b7401a15
henjue <henjue@gmail.com>
2015-08-18 12:17:11 +0800

    update

--- app/src/main/java/maimeng/yodian/app/client/android/view/WebViewActivity.java
+++ app/src/main/java/maimeng/yodian/app/client/android/view/WebViewActivity.java
@@ -1,21 +1,29 @@
 package maimeng.yodian.app.client.android.view;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.melnykov.fab.FloatingActionButton;
 
 import maimeng.yodian.app.client.android.R;
+import maimeng.yodian.app.client.android.view.skill.SkillDetailsActivity;
+import maimeng.yodian.app.client.android.view.user.UserHomeActivity;
 
 
 /**
@@ -24,6 +32,7 @@ import maimeng.yodian.app.client.android.R;
 public class WebViewActivity extends AbstractActivity {
 
     public static final String URL = "_url";
+    private View mBtnBack;
 
     public static Intent newIntent(Context context, String url) {
         Intent intent = new Intent(context, WebViewActivity.class);
@@ -46,7 +55,7 @@ public class WebViewActivity extends AbstractActivity {
 
         final String imgth = getIntent().getStringExtra(URL);
         final WebView web = (WebView) findViewById(R.id.web);
-        final View mBtnBack = findViewById(R.id.btn_back);
+        mBtnBack = findViewById(R.id.btn_back);
         ViewCompat.setTransitionName(mBtnBack, "back");
         mBtnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,9 +85,38 @@ public class WebViewActivity extends AbstractActivity {
                 super.onPageFinished(view, url);
             }
         };
-
+        web.addJavascriptInterface(new YoDianJavaScript(this),"yodian");
         web.setWebViewClient(webViewClient);
         web.loadUrl(imgth);
     }
 
+    public static class YoDianJavaScript{
+        private final Activity mContext;
+        public YoDianJavaScript(Activity context){
+            this.mContext=context;
+        }
+        @android.webkit.JavascriptInterface
+        public boolean startActivity(final String url){
+            if(url==null)return false;
+            Uri uri = Uri.parse(url);
+            if(uri.getScheme().equalsIgnoreCase("intent")){
+                String _type=uri.getQueryParameter("ydtype");
+                String _id=uri.getQueryParameter("ydid");
+                if(!TextUtils.isEmpty(_type) && !TextUtils.isEmpty(_id)){
+                    int type=Integer.parseInt(_type);
+                    long id=Long.parseLong(_id);
+                    switch (type){
+                        case 0:
+                            mContext.startActivity(new Intent(mContext,UserHomeActivity.class).putExtra("uid",id));
+                            return true;
+                        case 1:
+                            mContext.startActivity(new Intent(mContext,SkillDetailsActivity.class).putExtra("sid", id));
+                            return true;
+                    }
+                }
+
+            }
+            return false;
+        }
+    }
 }
