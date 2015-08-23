7a3337426680dad127d8f1d3f8abf03cf913ba9c
henjue <henjue@gmail.com>
2015-08-23 17:22:27 +0800

    update fix bug

--- app/src/main/java/maimeng/yodian/app/client/android/view/WebViewActivity.java
+++ app/src/main/java/maimeng/yodian/app/client/android/view/WebViewActivity.java
@@ -10,8 +10,10 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -50,10 +52,22 @@ public class WebViewActivity extends AbstractActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
-
+//        ActionBar actionBar = getSupportActionBar();
+//        if (actionBar != null) {
+//            actionBar.setHomeButtonEnabled(true);
+//            actionBar.setDisplayHomeAsUpEnabled(true);
+//        }
         final String imgth = getIntent().getStringExtra(URL);
         final WebView web = (WebView) findViewById(R.id.web);
         mBtnBack = findViewById(R.id.btn_back);
@@ -86,34 +100,38 @@ public class WebViewActivity extends AbstractActivity {
                 super.onPageFinished(view, url);
             }
         };
-        web.addJavascriptInterface(new YoDianJavaScript(this,web),"yodian");
+        web.addJavascriptInterface(new YoDianJavaScript(this, web), "yodian");
         web.setWebViewClient(webViewClient);
         web.loadUrl(imgth);
     }
 
-    public static class YoDianJavaScript{
+    public static class YoDianJavaScript {
         private final Activity mContext;
         private final WebView web;
 
-        public YoDianJavaScript(Activity context, WebView web){
-            this.mContext=context;
-            this.web=web;
+        public YoDianJavaScript(Activity context, WebView web) {
+            this.mContext = context;
+            this.web = web;
         }
+
         @android.webkit.JavascriptInterface
-        public boolean startActivity(final String url){
-            return WebLauncherUtils.handler(mContext,url);
+        public boolean startActivity(final String url) {
+            return WebLauncherUtils.handler(mContext, url);
         }
+
         @android.webkit.JavascriptInterface
-        public boolean canBack(){
+        public boolean canBack() {
             return web.canGoBack();
         }
+
         @android.webkit.JavascriptInterface
-        public void back(){
+        public void back() {
             web.goBack();
         }
+
         @android.webkit.JavascriptInterface
-        public void close(){
-                mContext.finish();
+        public void close() {
+            mContext.finish();
         }
     }
 }
