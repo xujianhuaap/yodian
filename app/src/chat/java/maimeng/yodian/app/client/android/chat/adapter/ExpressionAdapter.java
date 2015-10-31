6f5e57cd9e951f25cb28171b43cfae85640e2a06
henjue <henjue@gmail.com>
2015-10-31 21:02:40 +0800

    修复点击聊天表情程序fc

--- app/src/chat/java/maimeng/yodian/app/client/android/chat/adapter/ExpressionAdapter.java
+++ app/src/chat/java/maimeng/yodian/app/client/android/chat/adapter/ExpressionAdapter.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (C) 2013-2014 EaseMob Technologies. All rights reserved.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,26 +23,26 @@ import java.util.List;
 
 import maimeng.yodian.app.client.android.R;
 
-public class ExpressionAdapter extends ArrayAdapter<String>{
+public class ExpressionAdapter extends ArrayAdapter<String> {
 
-	public ExpressionAdapter(Context context, int textViewResourceId, List<String> objects) {
-		super(context, textViewResourceId, objects);
-	}
-	
-	
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		if(convertView == null){
-			convertView = View.inflate(getContext(), R.layout.row_expression, null);
-		}
-		
-		ImageView imageView = (ImageView) convertView.findViewById(R.id.iv_expression);
-		
-		String filename = getItem(position);
-		int resId = getContext().getResources().getIdentifier(filename, "drawable", getContext().getPackageName());
-		imageView.setImageResource(resId);
-		
-		return convertView;
-	}
-	
+    public ExpressionAdapter(Context context, int textViewResourceId, List<String> objects) {
+        super(context, textViewResourceId, objects);
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = View.inflate(getContext(), R.layout.row_expression, null);
+        }
+
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.iv_expression);
+
+        String filename = getItem(position);
+        int resId = getContext().getResources().getIdentifier(filename, "mipmap", getContext().getPackageName());
+        imageView.setImageResource(resId);
+
+        return convertView;
+    }
+
 }
