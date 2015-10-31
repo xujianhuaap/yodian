c85b555825e6fa1f7f66dcdb98d6d10dba87ad1c
henjue <henjue@gmail.com>
2015-10-31 17:35:25 +0800

    修改wechat支付流程控制
    当支付人时候如果没有安装微信人时候就提示用户没有安装微信

--- app/src/main/java/maimeng/yodian/app/client/android/view/deal/PayListActivity.java
+++ app/src/main/java/maimeng/yodian/app/client/android/view/deal/PayListActivity.java
@@ -1,8 +1,6 @@
 package maimeng.yodian.app.client.android.view.deal;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,8 +8,8 @@ import android.text.Html;
 import android.text.Spanned;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
@@ -19,24 +17,21 @@ import org.henjue.library.hnet.Callback;
 import org.henjue.library.hnet.HNet;
 import org.henjue.library.hnet.Response;
 import org.henjue.library.hnet.exception.HNetError;
-import org.json.JSONObject;
+import org.henjue.library.share.Type;
+import org.henjue.library.share.manager.AuthFactory;
+import org.henjue.library.share.manager.WechatAuthManager;
 
 import maimeng.yodian.app.client.android.BuildConfig;
 import maimeng.yodian.app.client.android.R;
-import maimeng.yodian.app.client.android.chat.activity.AlertDialog;
 import maimeng.yodian.app.client.android.model.OrderInfo;
 import maimeng.yodian.app.client.android.model.skill.Skill;
-import maimeng.yodian.app.client.android.model.user.User;
 import maimeng.yodian.app.client.android.network.ErrorUtils;
 import maimeng.yodian.app.client.android.network.Network;
-import maimeng.yodian.app.client.android.network.common.GsonConverter;
 import maimeng.yodian.app.client.android.network.common.RequestIntercept;
 import maimeng.yodian.app.client.android.network.response.RemainderPayParamsResponse;
-import maimeng.yodian.app.client.android.network.response.ToastResponse;
 import maimeng.yodian.app.client.android.network.response.WXPayParamResponse;
 import maimeng.yodian.app.client.android.network.response.ZhiFuBaoPayResponse;
 import maimeng.yodian.app.client.android.network.service.BuyService;
-import maimeng.yodian.app.client.android.utils.LogUtil;
 import maimeng.yodian.app.client.android.view.deal.pay.IPay;
 import maimeng.yodian.app.client.android.view.deal.pay.IPayStatus;
 import maimeng.yodian.app.client.android.view.deal.pay.RemainderFactory;
@@ -48,7 +43,7 @@ import maimeng.yodian.app.client.android.view.dialog.WaitDialog;
 /**
  * Created by xujianhua on 10/12/15.
  */
-public class PayListActivity extends AppCompatActivity implements View.OnClickListener{
+public class PayListActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView mTitle;
     private OrderInfo mInfo;
     private Skill mSkill;
@@ -57,49 +52,52 @@ public class PayListActivity extends AppCompatActivity implements View.OnClickLi
     private boolean isOrderPay;
     private WaitDialog mDialog;
 
-    private final static int PAY_TYPE_ZHIFUBAO=1;
-    private final static int PAY_TYPE_REMAINDER=3;
-    private final static int PAY_TYPE_WECHAT=2;
+    private final static int PAY_TYPE_ZHIFUBAO = 1;
+    private final static int PAY_TYPE_REMAINDER = 3;
+    private final static int PAY_TYPE_WECHAT = 2;
 
     /***
-     *订单支付
+     * 订单支付
+     *
      * @param context
      */
-    public  static void show(Activity context,OrderInfo orderInfo,int requestCode){
-        Intent intent=new Intent(context,PayListActivity.class);
+    public static void show(Activity context, OrderInfo orderInfo, int requestCode) {
+        Intent intent = new Intent(context, PayListActivity.class);
         intent.putExtra("orderInfo", orderInfo);
-        context.startActivityForResult(intent,requestCode);
+        context.startActivityForResult(intent, requestCode);
     }
 
     /***
      * 技能购买
+     *
      * @param context
      * @param orderInfo
      * @param requestCode
      */
-    public  static void show(Activity context,Skill orderInfo,int requestCode){
-        Intent intent=new Intent(context,PayListActivity.class);
+    public static void show(Activity context, Skill orderInfo, int requestCode) {
+        Intent intent = new Intent(context, PayListActivity.class);
         intent.putExtra("skill", orderInfo);
-        context.startActivityForResult(intent,requestCode);
+        context.startActivityForResult(intent, requestCode);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setGravity(Gravity.BOTTOM);
         setContentView(R.layout.activity_pay_list);
 
-        mTitle=(TextView)findViewById(R.id.pay_title);
+        mTitle = (TextView) findViewById(R.id.pay_title);
         findViewById(R.id.pay_wechat).setOnClickListener(this);
         findViewById(R.id.pay_zhifubao).setOnClickListener(this);
         findViewById(R.id.pay_remainer).setOnClickListener(this);
 
-        Intent intent=getIntent();
-        if(intent.hasExtra("orderInfo")){
+        Intent intent = getIntent();
+        if (intent.hasExtra("orderInfo")) {
             mOrderInfo = intent.getParcelableExtra("orderInfo");
             mTitle.setText(Html.fromHtml(getResources().getString(R.string.pay_list_title, mOrderInfo.getTotal_fee())));
-            isOrderPay=true;
-        }else{
-            mSkill=intent.getParcelableExtra("skill");
+            isOrderPay = true;
+        } else {
+            mSkill = intent.getParcelableExtra("skill");
             mTitle.setText(Html.fromHtml(getResources().getString(R.string.pay_list_title, mSkill.getPrice())));
         }
 
@@ -108,35 +106,38 @@ public class PayListActivity extends AppCompatActivity implements View.OnClickLi
                 .setEndpoint(BuildConfig.API_HOST)
                 .setIntercept(new RequestIntercept(this))
                 .build();
-        mService= net.create(BuyService.class);
+        mService = net.create(BuyService.class);
 
 
     }
 
 
-
     /***
-     *
      * @param v
      */
     @Override
     public void onClick(View v) {
         //生成订单信息
-        if(isOrderPay){
-            if(v.getId()==R.id.pay_remainer){
-                mService.buyOrder(mOrderInfo.getOid(),PAY_TYPE_REMAINDER,new CallBackProxy(PAY_TYPE_REMAINDER));
-            }else if(v.getId()==R.id.pay_wechat){
-                mService.buyOrder(mOrderInfo.getOid(), PAY_TYPE_WECHAT, new CallBackProxy(PAY_TYPE_WECHAT));
-            }else if(v.getId()==R.id.pay_zhifubao){
-                mService.buyOrder(mOrderInfo.getOid(),PAY_TYPE_ZHIFUBAO,new CallBackProxy(PAY_TYPE_ZHIFUBAO));
+        if (isOrderPay) {
+            if (v.getId() == R.id.pay_remainer) {
+                mService.buyOrder(mOrderInfo.getOid(), PAY_TYPE_REMAINDER, new CallBackProxy(PAY_TYPE_REMAINDER));
+            } else if (v.getId() == R.id.pay_wechat) {
+                WechatAuthManager manager = (WechatAuthManager) AuthFactory.create(this, Type.Platform.WEIXIN);//代码一定不能删除，保证IWXAPI已经初始化
+                if (manager.getIWXAPI().isWXAppInstalled()) {
+                    mService.buyOrder(mOrderInfo.getOid(), PAY_TYPE_WECHAT, new CallBackProxy(PAY_TYPE_WECHAT));
+                } else {
+                    Toast.makeText(this, "没有安装微信", Toast.LENGTH_SHORT).show();
+                }
+            } else if (v.getId() == R.id.pay_zhifubao) {
+                mService.buyOrder(mOrderInfo.getOid(), PAY_TYPE_ZHIFUBAO, new CallBackProxy(PAY_TYPE_ZHIFUBAO));
             }
-        }else {
-            if(v.getId()==R.id.pay_remainer){
+        } else {
+            if (v.getId() == R.id.pay_remainer) {
                 mService.buySkill(mSkill.getId(), PAY_TYPE_REMAINDER, new CallBackProxy(PAY_TYPE_REMAINDER));
-            }else if(v.getId()==R.id.pay_wechat){
-                mService.buySkill(mSkill.getId(),  PAY_TYPE_WECHAT, new CallBackProxy(PAY_TYPE_WECHAT));
-            }else if(v.getId()==R.id.pay_zhifubao){
-                mService.buySkill(mSkill.getId(),PAY_TYPE_ZHIFUBAO,new CallBackProxy(PAY_TYPE_ZHIFUBAO));
+            } else if (v.getId() == R.id.pay_wechat) {
+                mService.buySkill(mSkill.getId(), PAY_TYPE_WECHAT, new CallBackProxy(PAY_TYPE_WECHAT));
+            } else if (v.getId() == R.id.pay_zhifubao) {
+                mService.buySkill(mSkill.getId(), PAY_TYPE_ZHIFUBAO, new CallBackProxy(PAY_TYPE_ZHIFUBAO));
             }
         }
 
@@ -146,7 +147,7 @@ public class PayListActivity extends AppCompatActivity implements View.OnClickLi
     /***
      *
      */
-    public final class CallBackProxy implements Callback<String>{
+    public final class CallBackProxy implements Callback<String> {
         private final int payType;
 
         public CallBackProxy(int payType) {
@@ -155,51 +156,49 @@ public class PayListActivity extends AppCompatActivity implements View.OnClickLi
 
         @Override
         public void start() {
-            mDialog=WaitDialog.show(PayListActivity.this);
+            mDialog = WaitDialog.show(PayListActivity.this);
         }
 
         @Override
-        public void success(final  String s, Response response) {
+        public void success(final String s, Response response) {
             //执行支付
-             final Gson gson=Network.getOne().getGson();
-             IPay pay=null;
-             final IPayStatus status=new PayStatus();
-            if(payType==PAY_TYPE_ZHIFUBAO){
-                ZhiFuBaoPayResponse zhiFuBaoPayResponse=gson.fromJson(s, ZhiFuBaoPayResponse.class);
-                pay= ZhiFuBaoFactory.createInstance(PayListActivity.this,zhiFuBaoPayResponse.getData().getParams(),status);
+            final Gson gson = Network.getOne().getGson();
+            IPay pay = null;
+            final IPayStatus status = new PayStatus();
+            if (payType == PAY_TYPE_ZHIFUBAO) {
+                ZhiFuBaoPayResponse zhiFuBaoPayResponse = gson.fromJson(s, ZhiFuBaoPayResponse.class);
+                pay = ZhiFuBaoFactory.createInstance(PayListActivity.this, zhiFuBaoPayResponse.getData().getParams(), status);
                 pay.sendReq();
-            }else if(payType==PAY_TYPE_WECHAT){
-                WXPayParamResponse paramResponse=gson.fromJson(s, WXPayParamResponse.class);
-                pay= WXFactory.createInstance(PayListActivity.this,paramResponse.getData().getParams(),status);
+            } else if (payType == PAY_TYPE_WECHAT) {
+                WXPayParamResponse paramResponse = gson.fromJson(s, WXPayParamResponse.class);
+                pay = WXFactory.createInstance(PayListActivity.this, paramResponse.getData().getParams(), status);
                 pay.sendReq();
-            }else if(payType==PAY_TYPE_REMAINDER){
+            } else if (payType == PAY_TYPE_REMAINDER) {
 
-                 final android.support.v7.app.AlertDialog alertDialog=new ViewDialog.Builder(PayListActivity.this).setMesage(getResources().getString(R.string.pay_deal_tip))
+                final android.support.v7.app.AlertDialog alertDialog = new ViewDialog.Builder(PayListActivity.this).setMesage(getResources().getString(R.string.pay_deal_tip))
                         .setPositiveListener(new ViewDialog.IPositiveListener() {
                             @Override
                             public void positiveClick() {
-                                RemainderPayParamsResponse remainderPayParamsResponse=gson.fromJson(s, RemainderPayParamsResponse.class);
-                                IPay pay=RemainderFactory.createInstance(PayListActivity.this,remainderPayParamsResponse.getData().getOid(),status);
+                                RemainderPayParamsResponse remainderPayParamsResponse = gson.fromJson(s, RemainderPayParamsResponse.class);
+                                IPay pay = RemainderFactory.createInstance(PayListActivity.this, remainderPayParamsResponse.getData().getOid(), status);
                                 pay.sendReq();
                             }
-                        },"").setNegtiveListener(new ViewDialog.INegativeListener() {
-                    @Override
-                    public void negtiveClick() {
-                        finish();
-                    }
-                },"").create();
+                        }, "").setNegtiveListener(new ViewDialog.INegativeListener() {
+                            @Override
+                            public void negtiveClick() {
+                                finish();
+                            }
+                        }, "").create();
                 alertDialog.show();
 
             }
-
-
 
 
         }
 
         @Override
         public void failure(HNetError hNetError) {
-            ErrorUtils.checkError(PayListActivity.this,hNetError);
+            ErrorUtils.checkError(PayListActivity.this, hNetError);
         }
 
         @Override
@@ -211,16 +210,16 @@ public class PayListActivity extends AppCompatActivity implements View.OnClickLi
     /***
      * 支付结果
      */
-    public class PayStatus implements IPayStatus{
+    public class PayStatus implements IPayStatus {
         @Override
         public void failurepay(int errCode) {
             Spanned fail = Html.fromHtml(getResources().getString(R.string.pay_result_fail));
-            String failStr=fail.toString();
-            if(errCode==IPayStatus.PAY_ERROR_REMAINDER_SHORT){
-                failStr=getResources().getString(R.string.pay_deal_remainder_shortage);
+            String failStr = fail.toString();
+            if (errCode == IPayStatus.PAY_ERROR_REMAINDER_SHORT) {
+                failStr = getResources().getString(R.string.pay_deal_remainder_shortage);
             }
-            String btnTip=getResources().getString(R.string.btn_name);
-            String title=getResources().getString(R.string.pay_deal_title);
+            String btnTip = getResources().getString(R.string.btn_name);
+            String title = getResources().getString(R.string.pay_deal_title);
 
             new ViewDialog.Builder(PayListActivity.this).setMesage(failStr)
                     .setTitle(title).setPositiveListener(new ViewDialog.IPositiveListener() {
@@ -228,8 +227,7 @@ public class PayListActivity extends AppCompatActivity implements View.OnClickLi
                 public void positiveClick() {
                     finish();
                 }
-            }   ,btnTip).create().show();
-
+            }, btnTip).create().show();
 
 
         }
@@ -237,15 +235,15 @@ public class PayListActivity extends AppCompatActivity implements View.OnClickLi
         @Override
         public void sucessPay(int errCode) {
             Spanned sucess = Html.fromHtml(getResources().getString(R.string.pay_result_sucess));
-            String btnTip=getResources().getString(R.string.btn_name);
-            String title=getResources().getString(R.string.pay_deal_title);
+            String btnTip = getResources().getString(R.string.btn_name);
+            String title = getResources().getString(R.string.pay_deal_title);
             new ViewDialog.Builder(PayListActivity.this).setMesage(sucess.toString())
                     .setTitle(title).setPositiveListener(new ViewDialog.IPositiveListener() {
-                        @Override
-                        public void positiveClick() {
-                            finish();
-                        }
-                    },btnTip).create().show();
+                @Override
+                public void positiveClick() {
+                    finish();
+                }
+            }, btnTip).create().show();
         }
     }
 }
