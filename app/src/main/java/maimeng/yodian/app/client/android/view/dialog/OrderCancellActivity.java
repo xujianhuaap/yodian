d01b3a9f27ce350f9bfd14dab0afbae7271d4c74
xujianhuaap@gmail.com <xujianhuaap@gail.com>
2015-11-12 18:32:48 +0800

    ordercancel

--- app/src/main/java/maimeng/yodian/app/client/android/view/dialog/OrderCancellActivity.java
+++ app/src/main/java/maimeng/yodian/app/client/android/view/dialog/OrderCancellActivity.java
@@ -6,9 +6,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Toast;
+
+import org.henjue.library.hnet.Callback;
+import org.henjue.library.hnet.Response;
+import org.henjue.library.hnet.exception.HNetError;
 
 import maimeng.yodian.app.client.android.R;
 import maimeng.yodian.app.client.android.model.user.User;
+import maimeng.yodian.app.client.android.network.Network;
+import maimeng.yodian.app.client.android.network.response.ToastResponse;
+import maimeng.yodian.app.client.android.network.service.OrderService;
 import maimeng.yodian.app.client.android.view.deal.BasicalInfoConfirmActivity;
 import maimeng.yodian.app.client.android.view.deal.BindStatus;
 
@@ -23,8 +31,9 @@ public class OrderCancellActivity extends AppCompatActivity  {
      * @param context
      */
 
-    public static void show(Context context) {
+    public static void show(Context context,long oid) {
         Intent intent = new Intent(context, OrderCancellActivity.class);
+        intent.putExtra("oid",oid);
         context.startActivity(intent);
     }
 
@@ -46,6 +55,29 @@ public class OrderCancellActivity extends AppCompatActivity  {
             @Override
             public void onClick(View v) {
                 //取消订单
+                long oid=getIntent().getLongExtra("oid",0);
+                Network.getService(OrderService.class).cancleOrder(oid, new Callback<ToastResponse>() {
+                    @Override
+                    public void start() {
+
+                    }
+
+                    @Override
+                    public void success(ToastResponse toastResponse, Response response) {
+                        Toast.makeText(OrderCancellActivity.this,toastResponse.getMsg(),Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+
+                    @Override
+                    public void failure(HNetError hNetError) {
+
+                    }
+
+                    @Override
+                    public void end() {
+
+                    }
+                });
             }
         });
 
