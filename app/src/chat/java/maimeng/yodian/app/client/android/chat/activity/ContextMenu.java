63cd907481668dbcc72e47a956121efa5e3f77de
henjue <henjue@gmail.com>
2015-12-05 18:16:31 +0800

    update chat menu

--- app/src/chat/java/maimeng/yodian/app/client/android/chat/activity/ContextMenu.java
+++ app/src/chat/java/maimeng/yodian/app/client/android/chat/activity/ContextMenu.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (C) 2013-2014 EaseMob Technologies. All rights reserved.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,7 @@ package maimeng.yodian.app.client.android.chat.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -22,29 +23,30 @@ import com.easemob.chat.EMMessage;
 
 import maimeng.yodian.app.client.android.R;
 
-public class ContextMenu extends BaseActivity {
+public class ContextMenu extends AppCompatActivity {
 
-	private int position;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		int txtValue = EMMessage.Type.TXT.ordinal();
-		int type = getIntent().getIntExtra("type", -1);
-		if (type == EMMessage.Type.TXT.ordinal()) {
-		    setContentView(R.layout.context_menu_for_text);
-		} else if (type == EMMessage.Type.LOCATION.ordinal()) {
-		    setContentView(R.layout.context_menu_for_location);
-		} else if (type == EMMessage.Type.IMAGE.ordinal()) {
-		    setContentView(R.layout.context_menu_for_image);
-		} else if (type == EMMessage.Type.VOICE.ordinal()) {
-		    setContentView(R.layout.context_menu_for_voice);
-		} else if (type == EMMessage.Type.VIDEO.ordinal()) {
-			setContentView(R.layout.context_menu_for_video);
-		}
-		    
+    private int position;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("");
+        int txtValue = EMMessage.Type.TXT.ordinal();
+        int type = getIntent().getIntExtra("type", -1);
+        if (type == EMMessage.Type.TXT.ordinal()) {
+            setContentView(R.layout.context_menu_for_text);
+        } else if (type == EMMessage.Type.LOCATION.ordinal()) {
+            setContentView(R.layout.context_menu_for_location);
+        } else if (type == EMMessage.Type.IMAGE.ordinal()) {
+            setContentView(R.layout.context_menu_for_image);
+        } else if (type == EMMessage.Type.VOICE.ordinal()) {
+            setContentView(R.layout.context_menu_for_voice);
+        } else if (type == EMMessage.Type.VIDEO.ordinal()) {
+            setContentView(R.layout.context_menu_for_video);
+        }
+
 		/*    
-		switch (getIntent().getIntExtra("type", -1)) {
+        switch (getIntent().getIntExtra("type", -1)) {
 		case txtValue:
 			setContentView(R.layout.context_menu_for_text);
 			break;
@@ -68,39 +70,43 @@ public class ContextMenu extends BaseActivity {
 			break;
 		}
 		*/
-		position = getIntent().getIntExtra("position", -1);
-	}
+        position = getIntent().getIntExtra("position", -1);
+    }
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		finish();
-		return true;
-	}
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        finish();
+        return true;
+    }
 
-	public void copy(View view){
-		setResult(ChatActivity.RESULT_CODE_COPY, new Intent().putExtra("position", position));
-		finish();
-	}
-	public void delete(View view){
-		setResult(ChatActivity.RESULT_CODE_DELETE, new Intent().putExtra("position", position));
-		finish();
-	}
-	public void forward(View view){
-		setResult(ChatActivity.RESULT_CODE_FORWARD, new Intent().putExtra("position", position));
-		finish();
-	}
-	
-	public void open(View v){
-	    setResult(ChatActivity.RESULT_CODE_OPEN, new Intent().putExtra("position", position));
+    public void copy(View view) {
+        setResult(ChatActivity.RESULT_CODE_COPY, new Intent().putExtra("position", position));
         finish();
-	}
-	public void download(View v){
-	    setResult(ChatActivity.RESULT_CODE_DWONLOAD, new Intent().putExtra("position", position));
+    }
+
+    public void delete(View view) {
+        setResult(ChatActivity.RESULT_CODE_DELETE, new Intent().putExtra("position", position));
         finish();
-	}
-	public void toCloud(View v){
-	    setResult(ChatActivity.RESULT_CODE_TO_CLOUD, new Intent().putExtra("position", position));
+    }
+
+    public void forward(View view) {
+        setResult(ChatActivity.RESULT_CODE_FORWARD, new Intent().putExtra("position", position));
         finish();
-	}
-	
+    }
+
+    public void open(View v) {
+        setResult(ChatActivity.RESULT_CODE_OPEN, new Intent().putExtra("position", position));
+        finish();
+    }
+
+    public void download(View v) {
+        setResult(ChatActivity.RESULT_CODE_DWONLOAD, new Intent().putExtra("position", position));
+        finish();
+    }
+
+    public void toCloud(View v) {
+        setResult(ChatActivity.RESULT_CODE_TO_CLOUD, new Intent().putExtra("position", position));
+        finish();
+    }
+
 }
