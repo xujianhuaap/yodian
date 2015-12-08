c15b8de7b6056f6da2c44389fc19b894a5c1b09b
henjue <henjue@gmail.com>
2015-12-08 15:45:29 +0800

    update

--- app/src/main/java/maimeng/yodian/app/client/android/network/ErrorUtils.java
+++ app/src/main/java/maimeng/yodian/app/client/android/network/ErrorUtils.java
@@ -20,22 +20,25 @@ public class ErrorUtils {
             if (error.getKind() == HNetError.Kind.HTTP) {
                 postCatchedException(error);
                 LogUtil.e(ErrorUtils.class.getName(), error, "%s\n%d\n", error.getUrl(), error.getResponse().getStatus());
-                Toast.makeText(context, "连接服务器出错了...", Toast.LENGTH_SHORT).show();
+                if (context != null)
+                    Toast.makeText(context, "连接服务器出错了...", Toast.LENGTH_SHORT).show();
             } else if (error.getKind() == HNetError.Kind.NETWORK) {
                 if (error.getResponse() != null) {
                     LogUtil.e(ErrorUtils.class.getName(), "%s\n%d\n", error.getUrl(), error.getResponse().getStatus(), error);
                 }
-                Toast.makeText(context, "和服务器通讯失败,请检查网络...", Toast.LENGTH_SHORT).show();
+                if (context != null)
+                    Toast.makeText(context, "和服务器通讯失败,请检查网络...", Toast.LENGTH_SHORT).show();
             } else if (error.getKind() == HNetError.Kind.CONVERSION) {
                 postCatchedException(error);
                 LogUtil.e(ErrorUtils.class.getName(), error, "%s\n%d\n", error.getUrl(), error.getResponse().getStatus());
-                Toast.makeText(context, "不能处理服务器返回给我的数据....", Toast.LENGTH_SHORT).show();
+                if (context != null)
+                    Toast.makeText(context, "不能处理服务器返回给我的数据....", Toast.LENGTH_SHORT).show();
             } else {
                 postCatchedException(error);
                 if (error.getResponse() != null) {
                     LogUtil.e(ErrorUtils.class.getName(), error, "%s\n%d\n", error.getUrl(), error.getResponse().getStatus());
                 }
-                Toast.makeText(context, "运行出错啦...", Toast.LENGTH_SHORT).show();
+                if (context != null) Toast.makeText(context, "运行出错啦...", Toast.LENGTH_SHORT).show();
             }
 
         }
