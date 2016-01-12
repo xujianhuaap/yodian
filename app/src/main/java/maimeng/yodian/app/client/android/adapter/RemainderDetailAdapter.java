cddd8df1965ed54620f92b2cc167d1f91f7202a3
xujianhua <xujianhuaap@gmail.com>
2016-01-12 10:25:00 +0800

    ecllipse

--- app/src/main/java/maimeng/yodian/app/client/android/adapter/RemainderDetailAdapter.java
+++ app/src/main/java/maimeng/yodian/app/client/android/adapter/RemainderDetailAdapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
